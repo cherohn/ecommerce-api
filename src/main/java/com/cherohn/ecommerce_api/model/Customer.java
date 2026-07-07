cb5fb8c632c084cbc370217073f9d62c1e48b91f
cherohn <matheus.garcez09@gmail.com>
2026-07-07 00:57:47 -0300

    feat: adicionado entidade Customer

---
+++ src/main/java/com/cherohn/ecommerce_api/model/Customer.java
@@ -0,0 +1,33 @@
+package com.cherohn.ecommerce_api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String phone;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+}
